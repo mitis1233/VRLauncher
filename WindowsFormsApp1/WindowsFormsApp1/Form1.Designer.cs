b4986f653cf0b187eeaf5aad2168cb82f7436420
mtis1233 <mtis1233@gmail.com>
2021-03-31 20:37:14 +0800

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(2, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "VR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(101, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "FBT VR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(2, 60);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(199, 120);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Game";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(203, 184);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "VRlauncher";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //VR
+        {
+            Process process = new Process();
+            process.StartInfo.UseShellExecute = false; //必要引數
+            process.StartInfo.CreateNoWindow = true;
+            System.IO.Directory.SetCurrentDirectory("E:\\Game\\Steam\\steamapps\\common\\SteamVR\\bin\\win64");
+            process.StartInfo.FileName = "vrmonitor.exe";
+            process.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //FBT VR
+        {
+            Process process = new Process();
+            process.StartInfo.UseShellExecute = false; //必要引數
+            process.StartInfo.CreateNoWindow = true;
+            System.IO.Directory.SetCurrentDirectory("C:\\K2EX");
+            process.StartInfo.FileName = "KinectV1Process.exe";
+            process.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(int hwnd, int nCmdShow);
+        private enum CommandShow : int { SW_HIDE = 0, SW_SHOWNORMAL = 1, SW_NORMAL = 1,
+            SW_SHOWMINIMIZED = 2, SW_SHOWMAXIMIZED = 3, SW_MAXIMIZE = 3, SW_SHOWNOACTIVATE = 4,
+            SW_SHOW = 5, SW_MINIMIZE = 6, SW_SHOWMINNOACTIVE = 7, SW_SHOWNA = 8, SW_RESTORE = 9,
+            SW_SHOWDEFAULT = 10, SW_FORCEMINIMIZE = 11, SW_MAX = 11 };
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow([MarshalAs(UnmanagedType.LPTStr)] string lpClassName, [MarshalAs(UnmanagedType.LPTStr)] string lpWindowName);
+
+        private void button3_Click(object sender, EventArgs e) //Game
+        {
+            Process process = new Process();
+            process.StartInfo.UseShellExecute = false; //必要引數
+            process.StartInfo.RedirectStandardInput = true;//傳入引數設定
+            process.StartInfo.CreateNoWindow = true;
+            System.IO.Directory.SetCurrentDirectory("E:\\Game\\Steam\\steamapps\\common\\VRChat");
+            process.StartInfo.FileName = "VRChat.exe";
+            process.StartInfo.Arguments = "-screen-width 720 -screen-height 400";
+            process.Start();
+
+            Thread.Sleep(3000);
+            IntPtr Handle = FindWindow("Chrome_WidgetWin_1", "Oculus");
+            ShowWindow((int)Handle, (int)CommandShow.SW_MINIMIZE);
+            Handle = FindWindow(null, "SteamVR 狀態");
+            ShowWindow((int)Handle, (int)CommandShow.SW_MINIMIZE);
+            Handle = FindWindow(null, "VRChat");
+            ShowWindow((int)Handle, (int)CommandShow.SW_MINIMIZE);
+            Process[] p = Process.GetProcessesByName("chrome");
+            if (p.Length > 0)
+            {
+                for (int i = 0; i < p.Length; i++)
+                {
+                    int hwnd = p[i].MainWindowHandle.ToInt32();
+                    ShowWindow(hwnd, (int)CommandShow.SW_MINIMIZE);
+                    ShowWindow(hwnd, (int)CommandShow.SW_MAXIMIZE);
+                }
+            }
+            p = null;
+            System.Environment.Exit(System.Environment.ExitCode);
+        }
+    }
+}
